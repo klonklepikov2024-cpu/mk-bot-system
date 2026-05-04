f2a1268c58702467c2f2ee7539fd2c121cbfaf61
klonklepikov2024-cpu <klonklepikov2024@gmail.com>
2026-05-05 00:45:47 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -606,7 +606,6 @@ app = Flask(__name__)
 @app.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     json_string = request.get_data().decode('utf-8')
-    print(f"📥 ТЕЛЕГРАМ ПРИСЛАЛ: {json_string}") # <--- Эта строчка покажет всё!
     update = telebot.types.Update.de_json(json_string)
     bot.process_new_updates([update])
     return "!", 200
